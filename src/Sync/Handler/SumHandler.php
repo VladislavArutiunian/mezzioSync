64e5f3c6e89727460668625a7ae1c65b1492dbdf
varutyunyan <varutyunyan@team.amocrm.com>
2023-04-24 21:06:47 +0300

    Revert "gitignore added file"
    This reverts commit e6ae917397dd1f6a6bacc05e5f82c27bb52cd663.

--- src/Sync/Handler/SumHandler.php
+++ src/Sync/Handler/SumHandler.php
@@ -8,8 +8,6 @@ use Laminas\Diactoros\Response\HtmlResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
 
 class SumHandler implements RequestHandlerInterface
 {
@@ -17,18 +15,6 @@ class SumHandler implements RequestHandlerInterface
     {
         $getParams = $request->getQueryParams();
         $paramsSum = $this->getParamsSum($getParams);
-        $inputParams = implode(', ', array_values($getParams));
-
-        if (is_null($paramsSum)) {
-            $message = "Provided Get Params isn't type of integer";
-            $this->writeLog('error', $message, $inputParams, $paramsSum);
-
-            return new HtmlResponse("<p>Error: input data is not type of integer</p>");
-        }
-        $message = "Success sum operation";
-        $result = $paramsSum;
-        $this->writeLog('info', $message, $inputParams, $result);
-
         return new HtmlResponse("<p>Sum of params is $paramsSum</p>");
     }
 
@@ -42,27 +28,5 @@ class SumHandler implements RequestHandlerInterface
             $sum += $param;
         }
         return $sum;
-    }
-
-    public function writeLog(string $logType, string $message, string $inputParams, ?int $result)
-    {
-        $log = new Logger('sums-logger');
-        $currentDate = date('Y-m-d');
-        $stream = new StreamHandler(
-            dirname(__DIR__, 3) . "/log/$currentDate/request.log",
-            Logger::DEBUG);
-        $log->pushHandler($stream);
-
-        $output = "Message: $message; input params: $inputParams; result: $result";
-        switch ($logType) {
-            case "error":
-                $log->error($output);
-                break;
-            case "info":
-                $log->info($output);
-                break;
-            default:
-                throw new \Exception("Unknown type of log Type");
-        }
     }
 }
